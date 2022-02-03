deb679dabf2bcd2d703af813c9f0bdad07c94124
yogita <yogitasraut25@gmail.com>
2022-02-03 21:42:35 +0530

    Added: Part time employee wage using switch case

---
+++ Uc4_PartTimeEmployeeWageUsingSwitch.java
@@ -0,0 +1,32 @@
+package com.employeewage.practice;
+
+public class Uc4_PartTimeEmployeeWageUsingSwitch {
+	public static final int IS_FULL_TIME = 1;
+	public static final int IS_PART_TIME = 2;
+	public static final int IS_ABSENT = 0;
+	public static final int WAGE_PER_HOUR = 20;
+
+	public static void main(String[] args) {
+		int workHrs = 0;
+		int empCheck=(int)(Math.random()*10)%3;
+
+		switch(empCheck) {
+		case IS_FULL_TIME : workHrs=16;
+		System.out.println("Employee is full time present & working hours = "+workHrs);
+		break;
+		
+		case IS_PART_TIME : workHrs=8;
+		System.out.println("Employee is part time present & working hours = "+workHrs);
+		break;
+
+		case IS_ABSENT : workHrs=0;
+		System.out.println("Employee is absent & working hours = "+workHrs);
+		break;
+
+		default:System.out.println("Invalid Input");
+		}
+		System.out.println("Employee daily wage = "+(workHrs*WAGE_PER_HOUR));
+
+	}
+
+}
